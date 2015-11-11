77a732431f62fd4aaf589d70bae29f7e4aaa7899
Artem <vampirkod@gmail.com>
2015-11-12 01:32:10 +0200

    restructured project folders

---
+++ html/src/js/auth/RegisterController.js
@@ -0,0 +1,30 @@
+module.exports = ['$scope','$http','$state','AuthFactory',function($scope,$http,$state,AuthFactory) {
+  if(AuthFactory.isAuthorized()){
+    $state.go('group');
+  }
+
+  $scope.submit = submit;
+  var data = {};
+  function submit() {
+    console.log(this.loginForm);
+    if(this.loginForm.$valid){
+      data.email = this.loginForm.email;
+      data.password = this.loginForm.password;
+      //console.log(data);
+      //console.log($scope.token);
+      //$http.post('/auth/loginAjax',data)
+      //  .then(function(data) {
+      //    console.log(data);
+      //    if(data.data.code === 'success'){
+      //      AuthFactory.setAuthorize(true);
+      //      $state.go('group');
+      //    }else{
+      //      AuthFactory.setAuthorize(false);
+      //    }
+      //  });
+    }
+  }
+}];
+
+
+
