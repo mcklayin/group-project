c38717fea4c18b5cbe001c408e9f0a036d736a58
Artem <vampirkod@gmail.com>
2015-11-12 01:53:57 +0200

    add navigation links

--- html/dist/js/app.js
+++ html/dist/js/app.js
@@ -143,8 +143,36 @@ module.exports = function() {
 };
 
 },{}],8:[function(require,module,exports){
-module.exports = function() {
-  console.log('hif'); 
+module.exports = function(AuthFactory) {
+  this.links= {};
+  if(AuthFactory.isAuthorized()){
+    this.links = [
+      {
+        title: 'Група',
+        link: 'group'
+      },{
+        title: 'Кабінет',
+        link: 'cabinet'
+      },{
+        title: 'Вийти',
+        link: 'logout'
+      }
+    ];
+  }else{
+    this.links = [
+      {
+        title: 'Контакти',
+        link: 'contacts'
+      },{
+        title: 'Вхід',
+        link: 'login'
+      },{
+        title: 'Реєстрація',
+        link: 'register'
+      }
+    ];
+  }
+  
 };
 
 },{}],9:[function(require,module,exports){
--- html/src/js/nav/navigationCtrl.js
+++ html/src/js/nav/navigationCtrl.js
@@ -1,3 +1,31 @@
-module.exports = function() {
-  console.log('hif'); 
+module.exports = function(AuthFactory) {
+  this.links= {};
+  if(AuthFactory.isAuthorized()){
+    this.links = [
+      {
+        title: 'Група',
+        link: 'group'
+      },{
+        title: 'Кабінет',
+        link: 'cabinet'
+      },{
+        title: 'Вийти',
+        link: 'logout'
+      }
+    ];
+  }else{
+    this.links = [
+      {
+        title: 'Контакти',
+        link: 'contacts'
+      },{
+        title: 'Вхід',
+        link: 'login'
+      },{
+        title: 'Реєстрація',
+        link: 'register'
+      }
+    ];
+  }
+  
 };
